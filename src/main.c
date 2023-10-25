e71b955e6642799c3c8345c801c11bb0648a95fa
Newpson <dk.newpson@gmail.com>
2023-10-25 17:37:18 +0300

    New blinking algorithm

--- src/main.c
+++ src/main.c
@@ -3,16 +3,38 @@
 #include "nrf_delay.h"
 #include "boards.h"
 
+#define LED 0
+#define DELAY 500
+#define DEVICE_ID 6598
+
+#define BLINKCNT_0 ((DEVICE_ID)/1000)
+#define BLINKCNT_1 ((DEVICE_ID)/100%10)
+#define BLINKCNT_2 ((DEVICE_ID)/10%10)
+#define BLINKCNT_3 ((DEVICE_ID)/10%10)
+
+static inline
+void blink(int8_t cnt)
+{
+	for (; cnt > 0; --cnt)
+	{
+		bsp_board_led_invert(LED);
+		nrf_delay_ms(DELAY);
+		bsp_board_led_invert(LED);
+		nrf_delay_ms(DELAY);
+	}
+}
+
 int main(void)
 {
 	bsp_board_init(BSP_INIT_LEDS);
+	int8_t blinkcnt[] = {BLINKCNT_0, BLINKCNT_1, BLINKCNT_2, BLINKCNT_3};
 
 	while (true)
 	{
-		for (int i = 0; i < LEDS_NUMBER; ++i)
+		for (int8_t i = 0; i < 4; ++i)
 		{
-			bsp_board_led_invert(i);
-			nrf_delay_ms(500);
+			blink(blinkcnt[i]);
+			nrf_delay_ms(4*DELAY);
 		}
 	}
 }
